969681eb37b3a2d1b74107d600057642075b2f81
yonemoto <yonemoto@tam-tam.co.jp>
2023-09-16 18:31:49 +0900

    Add: Swr

--- src/hooks/useFetchRss.ts
+++ src/hooks/useFetchRss.ts
@@ -1,40 +1,59 @@
-import { useEffect, useState } from "react";
+import useSWR, { type SWRResponse } from "swr";
 
-interface Article {
+type Article = {
   title: string[];
   link: string[];
   "media:thumbnail": string[];
-}
+};
+
+type FetcherResponse = {
+  rss: {
+    channel: [
+      {
+        item: Article[];
+      }
+    ];
+  };
+};
+
+const fetcher = async (url: string): Promise<Article[]> => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Failed to fetch: ${res.statusText}`);
+  }
+
+  const data: FetcherResponse = await res.json();
+
+  if (Array.isArray(data.rss.channel[0].item)) {
+    return data.rss.channel[0].item;
+  } else {
+    throw new Error("Articles is not an array.");
+  }
+};
 
 /**
  * useFetchRss はRSSフィードから記事を取得するためのカスタムReactフックです。
  *
  * @returns オブジェクトを返します:
  * - `articles`: `Article`オブジェクトの配列。各オブジェクトは個々の記事を表します。
- * - `error`: エラーが発生した場合はErrorオブジェクト、それ以外はnull。
+ * - `error`: エラーが発生した場合はErrorオブジェクト、それ以外はundefined。
+ * - `isLoading`: データの読み込み中はtrue、それ以外はfalse。
  */
-export const useFetchRss = () => {
-  const [articles, setArticles] = useState<Article[]>([]);
-  const [error, setError] = useState<Error | null>(null);
+export const useFetchRss = (): {
+  articles: Article[] | undefined;
+  error: Error | undefined;
+  isLoading: boolean;
+} => {
+  const { data, error }: SWRResponse<Article[], Error> = useSWR(
+    "/.netlify/functions/fetch-note-rss",
+    fetcher
+  );
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch("/.netlify/functions/fetch-note-rss");
-        const data = await res.json();
+  const isLoading = !data && !error;
 
-        if (Array.isArray(data.rss.channel[0].item)) {
-          setArticles(data.rss.channel[0].item);
-        } else {
-          throw new Error("articles is not an array.");
-        }
-      } catch (e) {
-        setError(e);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  return { articles, error };
+  return {
+    articles: data,
+    error,
+    isLoading,
+  };
 };
